9182030ef746c0febdc5e3dabfde34148825ab85
Ignacio Salas R <146854109+Nacho-SR@users.noreply.github.com>
2024-05-13 17:57:59 -0600

    importando componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BodyComponent } from './components/app-body/body/body.component';
+import { HeaderComponent } from './components/app-header/header/header.component';
+import { FooterComponent } from './components/app-footer/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, BodyComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
