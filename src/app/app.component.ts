3cf4d7f058f8c8af3479a2215ebf2db727b41771
Ignacio Salas R <146854109+Nacho-SR@users.noreply.github.com>
2024-05-13 23:14:05 -0600

    Agregamos material y empezamos disenio login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { BodyComponent } from './components/app-body/body/body.component';
 import { HeaderComponent } from './components/app-header/header/header.component';
 import { FooterComponent } from './components/app-footer/footer/footer.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, BodyComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, BodyComponent, FooterComponent, RouterOutlet, MatGridListModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/app-body/body/body.component.ts
+++ src/app/components/app-body/body/body.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule, MatCardModule],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
