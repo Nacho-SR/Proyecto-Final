0d40ccee94ad0951cb7cb05ef543526d1c567ceb
Ignacio Salas R <146854109+Nacho-SR@users.noreply.github.com>
2024-05-14 15:56:35 -0600

    Agregando services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BodyComponent } from './components/app-body/body/body.component';
 import { HeaderComponent } from './components/app-header/header/header.component';
 import { FooterComponent } from './components/app-footer/footer/footer.component';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
+import { ObservablesService } from './services/observables.service';
+import { ObtenerDatosService } from './services/obtener-datos.service';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +16,33 @@ import {MatCardModule} from '@angular/material/card';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  valores:any[]=[];
+  checkoutForm: any;
+  loader:boolean = false;
+
+  constructor(private obtenerDatosService: ObtenerDatosService,private formBuilder: FormBuilder, private observablesService:ObservablesService ){
+    this.checkoutForm = this.formBuilder.group({
+      name: new FormControl(null, [Validators.minLength(1)]),
+      url: new FormControl(null,[Validators.maxLength(2)]),
+      email: new FormControl(null,[Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)])
+    });
+
+  }
+  ngOnInit(): void {
+    this.observablesService.loaderObs.subscribe((valor:boolean) => {
+      this.loader=valor;
+      console.log(valor);
+    });
+  }
+
+  onSubmit(valorFormulario:any): void {
+    if((this.checkoutForm.invalid) || (this.checkoutForm.get('name').value == 'angular')){
+      alert('Formulario incorrecto');
+    }else {
+      this.valores.push(valorFormulario);
+      this.checkoutForm.reset();
+    }
+  }
   title = 'Proyecto_Final';
 }
