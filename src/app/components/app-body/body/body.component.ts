d8c7888042b1ed1f05c297f5852ec0f51cd860fb
Ignacio Salas R <146854109+Nacho-SR@users.noreply.github.com>
2024-05-14 01:06:43 -0600

    Terminando tarjeta login

--- src/app/components/app-body/body/body.component.ts
+++ src/app/components/app-body/body/body.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [MatGridListModule, MatCardModule],
+  imports: [MatGridListModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
